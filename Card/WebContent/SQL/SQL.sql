008362bd71e6af354b414e064b73243191000552
tpdnd1012 <64762466+tpdnd1012@users.noreply.github.com>
2021-04-15 21:43:59 +0900

    Update SQL.sql

--- Card/WebContent/SQL/SQL.sql
+++ Card/WebContent/SQL/SQL.sql
@@ -4,12 +4,12 @@ create database Portfolio;	// SQL Database 생성
 
 create table Portfolio.Card( // 카드
 	card_number int not null primary key,		// 카드번호
-	card_name varchar(100) not null,			// 카드명
-	card_company varchar(100) not null,			// 카드회사
-	recommendation int,							// 추천
-	membership_fee int not null,				// 연회비
-	image										// 이미지
-	link varchar(1000) not null					// 발급사 링크
+	card_name varchar(100) not null,		// 카드명
+	card_company varchar(100) not null,		// 카드회사
+	recommendation int,				// 추천
+	membership_fee int not null,			// 연회비
+	image						// 이미지
+	link varchar(1000) not null			// 발급사 링크
 	
 );
 
@@ -17,8 +17,8 @@ create table Portfolio.Card( // 카드
 
 create table Portfolio.Card_company( // 카드사
 	card_company varchar(100) not null primary key,		// 카드회사
-	card_name varchar(100) not null,					// 카드명
-	card_category varchar not null						// 카테고리
+	card_name varchar(100) not null,			// 카드명
+	card_category varchar not null				// 카테고리
 );
 
 
@@ -27,5 +27,5 @@ create table Portfolio.Bulletin_board( // 게시판
 	board_num int not null primary key,		// 게시물 번호
 	card_name varchar(100) not null,		// 카드 
 	review varchar(1000) not null			// 후기 내용
-	star_score								// 별점
+	star_score					// 별점
 );
