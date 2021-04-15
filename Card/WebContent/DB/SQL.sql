a754a34eaa46464358cf422432c1e4b19f4a048e
kimse-ung <kimse-ung@218.38.137.28>
2021-04-16 01:03:59 +0900

    04-16 SQL

--- Card/WebContent/DB/SQL.sql
+++ Card/WebContent/DB/SQL.sql
@@ -1,28 +1,28 @@
 create database Portfolio;
 
 create table Card_company( // 카드사
-	card_company varchar(100) not null primary key,	// 카드회사
-	card_name varchar(100) not null,				// 카드명
-	card_category varchar(100) not null,			// 카테고리
+	card_company varchar(100) not null primary key,  // 카드회사
+	card_name varchar(100) not null,                 // 카드명
+	card_category varchar(100) not null,             // 카테고리
 );
 
 
 create table Card( // 카드
-	card_number int auto_increment primary key,	// 카드번호
-	card_name varchar(100) not null,			// 카드명
-	card_company varchar(100) not null,			// 카드회사
-	recommendation int,							// 추천수
-	membership_fee int not null,				// 연회비
-	images varchar(100),						// 이미지(링크)
-	bank_link varchar(1000) not null,			// 발급사 링크
+	card_number int auto_increment primary key,  // 카드번호
+	card_name varchar(100) not null,             // 카드명
+	card_company varchar(100) not null,          // 카드회사
+	recommendation int,                          // 추천수
+	membership_fee int not null,                 // 연회비
+	images varchar(100),                         // 이미지(링크)
+	bank_link varchar(1000) not null,            // 발급사 링크
 	foreign key (card_company) references Card_company (card_company)
 );
 
 
 create table Bulletin_board( // 게시판
-	board_num int not null primary key,	// 게시물 번호
-	card_name varchar(100) not null,	// 카드명
-	review varchar(1000) not null,		//후기내용
-	star_score float,					// 별점
+	board_num int not null primary key,  // 게시물 번호
+	card_name varchar(100) not null,     // 카드명
+	review varchar(1000) not null,       //후기내용
+	star_score float,                    // 별점
 	foreign key (card_name) references Card (card_name)
 );
