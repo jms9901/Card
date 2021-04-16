bbaaca693706d73eab0cf77178f7f824fc1afe5b
user <user@SAMSUNG-PC>
2021-04-16 18:56:36 +0900

    111

--- Card/WebContent/DB/SQL.sql
+++ Card/WebContent/DB/SQL.sql
@@ -1,28 +1,55 @@
 create database Portfolio;
 
-create table Card_company( // 카드사
-	card_company varchar(100) not null primary key,  // 카드회사
-	card_name varchar(100) not null,                 // 카드명
-	card_category varchar(100) not null,             // 카테고리
+create table Card_company( 
+	card_company varchar(100) not null primary key,  
+	card_name varchar(100) not null,                
+	card_category varchar(100) not null,             
 );
 
 
 create table Card( // 카드
-	card_number int auto_increment primary key,  // 카드번호
-	card_name varchar(100) not null,             // 카드명
-	card_company varchar(100) not null,          // 카드회사
-	recommendation int,                          // 추천수
-	membership_fee int not null,                 // 연회비
-	images varchar(100),                         // 이미지(링크)
-	bank_link varchar(1000) not null,            // 발급사 링크
+	card_number int auto_increment primary key, 
+	card_name varchar(100) not null,             
+	card_company varchar(100) not null,          
+	recommendation int,                         
+	membership_fee int not null,                 
+	images varchar(100),                         
+	bank_link varchar(1000) not null,           
 	foreign key (card_company) references Card_company (card_company)
 );
 
 
-create table Bulletin_board( // 게시판
-	board_num int not null primary key,  // 게시물 번호
-	card_name varchar(100) not null,     // 카드명
-	review varchar(1000) not null,       //후기내용
-	star_score float,                    // 별점
+create table Bulletin_board( 
+	board_num int not null primary key, 
+	card_name varchar(100) not null,     
+	review varchar(1000) not null,      
+	star_score float,                   
 	foreign key (card_name) references Card (card_name)
 );
+
+create table card.member(
+
+member_point int not null,
+member_id varchar(50) not null , 
+member_password varchar(40) not null,
+member_name varchar(3000) not null,
+member_resdentnum int primary key AUTO_INCREMENT,
+member_sex varchar(40) not null,
+member_birth varchar(100) not null,
+member_phone int not null
+
+/*member_cardcompany varchar(600) not null,
+member_cardbenefit varchar(600) not null,
+member_cardtype varchar (600) not null*/
+
+
+
+);
+
+create table card.interest(
+
+interest_num int primary key AUTO_INCREMENT,
+member_name varchar(50) not null , 
+card_card varchar(50) not null 
+
+)
